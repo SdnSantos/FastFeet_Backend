d9109633a810de9220a1e6b95d8c05f20830f18f
Sidney Santos <sidney_ssantos@hotmail.com>
2020-02-17 23:06:34 -0300

    Implementando o RecipientController

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 
-import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/users', UserController.index);
-routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users/:userId', UserController.update);
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:recipientId', RecipientController.update);
 
 export default routes;

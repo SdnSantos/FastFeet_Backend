9ff46e035554c31e206975c14a1d6a60d02b3065
Sidney Santos <sidney_ssantos@hotmail.com>
2020-02-22 17:30:56 -0300

    Inicio da criaçao da gestao de entregas

---
+++ src/app/controllers/DeliveryController.js
@@ -0,0 +1,10 @@
+import Delivery from '../models/Delivery';
+
+class DeliveryController () {
+  async store(req, res) {
+    return res.json();
+  }
+
+}
+
+export default new DeliveryController();
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -20,5 +26,9 @@ routes.get('/deliverymans', DeliverymanController.index);
 routes.post('/deliverymans', DeliverymanController.store);
 routes.put('/deliverymans/:deliverymanId', DeliverymanController.update);
 routes.delete('/deliverymans/:deliverymanId', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/deliveries', DeliveryController.store);
 
 export default routes;
